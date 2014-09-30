8360f7dd3b1abe7d966a80214aa624b0eb5e5bed
Dmitry Vorotilin <d.vorotilin@gmail.com>
2014-10-01 00:22:44 +0400

    Remove if file exists

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -38,6 +38,8 @@ class ActiveSupport::TestCase
   end
 
   def teardown
-    Dir[File.join(TMP_PATH, '*')].each { |f| File.delete(f) }
+    Dir[File.join(TMP_PATH, '*')].each do |file|
+      File.delete(file) if File.exists?(file)
+    end
   end
 end
